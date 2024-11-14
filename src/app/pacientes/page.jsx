8b41009a9e4f480096bab6d82f2b25117858d89f
abecerraguz <alejandrobecerrabecerra73@gmail.com>
2024-11-14 17:18:42 -0300

    Se creo el componente Paciente, dentro del page de paciente

--- src/app/pacientes/page.jsx
+++ src/app/pacientes/page.jsx
@@ -1,12 +1,13 @@
 "use client";
 import { useState, useEffect } from 'react';
-import Paciente from '@/components/Paciente';
+// import Paciente from '@/components/Paciente';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faCircleXmark, faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import ModalNewPacient from '@/components/ModalNewPaciente';
 import { nanoid } from 'nanoid';
 import ModalAlert from "@/components/ModalAlert";
 import ModalEditPaciente from '@/components/ModalEditPaciente';
+import ModalNewCita from '@/components/ModalNewCita';
 
 
 
@@ -19,6 +20,8 @@ function PagePacientes() {
     const [isModalEditar, setisModalEditar] = useState(false);
     const [estado, setEstado] = useState(null);
     const [expandido, setExpandido] = useState({});
+    const [isModalNewCita, setisModalNewCita] = useState(false);
+    const [citas, setCitas] = useState([]);
 
     useEffect(() => {
 
@@ -40,10 +43,50 @@ function PagePacientes() {
             }
         }
         getPacientes()
+        getCitas()
+        
     }, []);
 
 
+    const getCitas = async () => {
+        try {
+            const response = await fetch(`/api/citas`, {
+                method: 'GET',
+            });
 
+            if (response.ok) {
+                const data = await response.json()
+                const arrCitas = []
+                const opciones = {
+                    weekday: 'long',  // Día de la semana completo (Ej: "Lunes")
+                    year: 'numeric',  // Año (Ej: "2024")
+                    month: 'long',    // Mes completo (Ej: "Septiembre")
+                    day: 'numeric'    // Día del mes (Ej: "31")
+                };
+                data.forEach(element => {
+                    const { pk_idCita, fk_idPaciente, fecha, hora, agendar_citas: [{ consultorio, especialista: { nombre, apellido, especialidad } }] } = element
+                    const fechaActual = new Date(fecha);
+                    const fechaFormateada = fechaActual.toLocaleDateString('es-ES', opciones);
+                    arrCitas.push({
+                        pk_idCita,
+                        fk_idPaciente,
+                        nombre,
+                        apellido,
+                        especialidad,
+                        fecha: fechaFormateada,
+                        hora,
+                        consultorio
+                    })
+                });
+                setCitas(arrCitas)
+            } else {
+                console.error('Error al obtener registro');
+            }
+
+        } catch (error) {
+            console.error('Error al obtener el registro:', error);
+        }
+    }
 
     const handleAgregarPaciente = () => {
         setIsModalOpenNewPacient(true)
@@ -97,8 +140,8 @@ function PagePacientes() {
 
             if (response.ok) {
                 response.json()
-                    .then( response => {
-                        console.log('Salida de response en el page-->',  response  )
+                    .then(response => {
+                        console.log('Salida de response en el page-->', response)
                         const dataPaciente = [
                             ...pacientes,
                             response
@@ -114,9 +157,9 @@ function PagePacientes() {
         }
     };
 
-    const handleUpdateExpediente = async ( expedienteUpdate ) => {
-        console.log('expedienteUpdate en page-->', expedienteUpdate )
-        console.log('expedienteUpdate.expediente en page-->', expedienteUpdate.expediente )
+    const handleUpdateExpediente = async (expedienteUpdate) => {
+        console.log('expedienteUpdate en page-->', expedienteUpdate)
+        console.log('expedienteUpdate.expediente en page-->', expedienteUpdate.expediente)
         try {
             await fetch(`/api/expedientes/`, {
                 method: 'POST',
@@ -125,13 +168,13 @@ function PagePacientes() {
                 },
                 body: JSON.stringify(expedienteUpdate.expediente),
             })
-            .then(response =>{
-                if (response.ok) {
-                    closeModal();
-                } else {
-                    console.error('Error al crear expediente');
-                }
-            })
+                .then(response => {
+                    if (response.ok) {
+                        closeModal();
+                    } else {
+                        console.error('Error al crear expediente');
+                    }
+                })
 
         } catch (error) {
             console.error('Error al eliminar el registro:', error);
@@ -177,26 +220,26 @@ function PagePacientes() {
     const handleDelete = async (dataId) => {
 
         try {
-          
+
             const { id } = dataId
-            console.log('ID-->', id )
+            console.log('ID-->', id)
             await fetch(`/api/pacientes/${id}`, {
                 method: 'DELETE',
                 headers: {
                     'Content-Type': 'application/json',
                 }
             })
-            .then(response=> {
-                if(response.ok){
-                    response.json()
+                .then(response => {
+                    if (response.ok) {
+                        response.json()
                             .then(respuesta => {
-                                console.log('Salida de repuesta --->',respuesta)
+                                console.log('Salida de repuesta --->', respuesta)
                                 const filtro = pacientes.filter(element => element.pk_idPaciente !== respuesta.pk_idPaciente)
-                                setPacientes(filtro );
+                                setPacientes(filtro);
                                 closeModal();
                             })
-                }
-            })
+                    }
+                })
 
         } catch (error) {
             console.error('Error al eliminar el registro:', error);
@@ -226,7 +269,7 @@ function PagePacientes() {
     }
 
     // Abre la modal de editar paciente
-    const handleEditClick = ( paciente ) => {
+    const handleEditClick = (paciente) => {
         setisModalEditar(true);
         setPacienteSeleccionado(paciente);
 
@@ -245,6 +288,162 @@ function PagePacientes() {
         }
     };
 
+    const handleAddCita = () => {
+        setisModalNewCita(true)
+    };
+
+
+    const Paciente = ({ element }) => {
+        return (
+            <>
+                <tr>
+                    <th scope="row" className="collapseAction flex items-center justify-center">
+                        <i
+                            className={expandido[element.pk_idPaciente] ? 'minus' : 'more'}
+                            onClick={() => toggleExpandir(element.pk_idPaciente)}
+                        ></i>
+                    </th>
+                    <td>{element.nombre}</td>
+                    <td>{element.apellido}</td>
+                    <td>{element.sexo}</td>
+                    <td>{new Date(element.fechaNacimiento).toLocaleDateString()}</td>
+                    <td>{element.region}</td>
+                    <td>{element.ciudad}</td>
+                    <td>{element.telefono}</td>
+                    <td>
+                        <div className="flex gap-2">
+                            <input type="checkbox" className="toggle toggle-lg"
+                                checked={estado}
+                                onChange={() => handleEstadoChange}
+                                id={element.pk_idPaciente}
+                            />
+                            <button
+                                className="btn btn-sm btn-success hover:bg-cyan-600 text-slate-900 hover:text-white sm:w-1/4 text-xs md:text-sm tooltip flex"
+                                onClick={() => handleEditClick(element)}
+                                data-tip="Editar"
+                            >
+                                <FontAwesomeIcon
+                                    icon={faPenToSquare}
+                                    className="fa-regular fa-pen-to-square"
+                                    style={{ fontSize: 16 }}
+                                />
+                            </button>
+                            <button
+                                className="btn btn-sm btn-error hover:bg-red-900 text-black hover:text-white sm:w-1/4 text-xs md:text-sm tooltip flex"
+                                onClick={() => getPacienteID(element.pk_idPaciente)}
+                                data-tip="Eliminar Paciente"
+                            >
+                                <FontAwesomeIcon
+                                    icon={faCircleXmark}
+                                    className="fa-regular fa-circle-xmark"
+                                    style={{ color: 'white', fontSize: 16 }}
+                                />
+                            </button>
+                        </div>
+                    </td>
+
+                </tr>
+
+                <tr className={expandido[element.pk_idPaciente] ? 'bg-gradient-to-r from-gray-700 via-gray-900 to-slate-900' : 'bg-gradient-to-r from-gray-700 via-gray-900 to-slate-900 hidden'}>
+                    <td colSpan="10">
+                        <div className={`contentAction ${element.pk_idPaciente} flex gap-2`}>
+                            {/* <button
+    className="btn btn-sm bg-gradient-to-r from-green-500 to-green-700 font-normal tooltip flex"
+    onClick={handleEditPaciente}
+    data-info={pk_idPaciente}
+    data-tip="Eliminar"
+    >
+    <FontAwesomeIcon
+    icon={faCircleXmark}
+    className="fa-regular fa-circle-xmark"
+    style={{ color: 'white', fontSize: 16 }}
+    />
+    Editar especialista
+    </button> */}
+                            {/* <button
+    className="btn btn-sm bg-gradient-to-r from-green-500 to-green-700 font-normal tooltip flex"
+    onClick={handleEditExpedient}
+    data-info={pk_idPaciente}
+    data-tip="Eliminar"
+    >
+    <FontAwesomeIcon
+    icon={faCircleXmark}
+    className="fa-regular fa-circle-xmark"
+    style={{ color: 'white', fontSize: 16 }}
+    />
+    Editar expediente paciente
+    </button> */}
+                            <button
+                                className="btn btn-sm bg-gradient-to-r from-green-500 to-green-700 font-normal tooltip flex"
+                                onClick={handleAddCita}
+                                data-tip="Agendar cita"
+                            >
+                                <FontAwesomeIcon
+                                    icon={faCircleXmark}
+                                    className="fa-regular fa-circle-xmark"
+                                    style={{ color: 'white', fontSize: 16 }}
+                                />
+                                Agendar cita
+                            </button>
+                        </div>
+                        <div className={`contentInfoTableAgendarCita ${element.pk_idPaciente}`} infotable={element.pk_idPaciente}>
+                            <div className="block py-2 px-4 rounded-t-lg bg-gradient-to-r from-slate-900 to-slate-800 uppercase mt-3">Citas agendadas</div>
+                            <table className="table text-base">
+                                <thead className="text-base text-white uppercase font-normal md:text-base bg-slate-800">
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Doctor</th>
+                                        <th>Especialidad</th>
+                                        <th>Fecha</th>
+                                        <th>Hora</th>
+                                        <th>Consultorio</th>
+                                        <th>Acción</th>
+                                    </tr>
+                                </thead>
+                                <tbody className="infoTableCitas">
+                                    {citas.map((element, index) => (
+                                        <>  {element.fk_idPaciente == pk_idPaciente ?
+
+                                            <tr key={nanoid()}>
+                                                <td>{index + 1}</td>
+                                                <td>{element.nombre} {element.apellido}</td>
+                                                <td>{element.especialidad}</td>
+                                                <td>{element.fecha}</td>
+                                                <td>{element.hora}</td>
+                                                <td>{element.consultorio}</td>
+                                                <td>
+                                                    <button
+                                                        className="btn btn-sm btn-error hover:bg-red-900 text-black hover:text-white sm:w-full text-xs md:text-sm tooltip flex"
+                                                        onClick={() => handleAlertModalEliminarCita(element.pk_idCita)}
+                                                        // data-info={element.pk_idCita}
+                                                        data-tip="Eliminarrrrr"
+                                                    >
+                                                        <FontAwesomeIcon
+                                                            icon={faCircleXmark}
+                                                            className="fa-regular fa-circle-xmark"
+                                                            style={{ color: 'white', fontSize: 16 }}
+                                                        />
+                                                    </button>
+                                                </td>
+                                            </tr>
+
+
+
+                                            :
+                                            null
+                                        }
+
+                                        </>
+                                    ))}
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </td>
+                </tr>
+            </>
+        )
+    }
 
     return (
         <>
@@ -280,68 +479,9 @@ function PagePacientes() {
                             </tr>
                         </thead>
                         <tbody>
-
                             {pacientes.map(element => (
-                                // <Paciente
-                                //     paciente={element}
-                                //     key={nanoid()}
-                                //     onDelete={handleDelete}
-                                //     onUpdate={handleEditUpdate}
-                                // />
-                               
-                                <tr key={nanoid()}>
-                                    <th scope="row" className="collapseAction flex items-center justify-center">
-                                        <i
-                                            className={expandido[element.pk_idPaciente] ? 'minus' : 'more'}
-                                            onClick={() => toggleExpandir(element.pk_idPaciente)}
-                                        ></i>
-                                    </th>
-                                    <td>{element.nombre}</td>
-                                    <td>{element.apellido}</td>
-                                    <td>{element.sexo}</td>
-                                    <td>{new Date(element.fechaNacimiento).toLocaleDateString()}</td>
-                                    <td>{element.region}</td>
-                                    <td>{element.ciudad}</td>
-                                    <td>{element.telefono}</td>
-                                    <td>
-                                        <div className="flex gap-2">
-                                            <input type="checkbox" className="toggle toggle-lg"
-                                                checked={estado}
-                                                onChange={() => handleEstadoChange}
-                                                id={element.pk_idPaciente}
-                                            />
-                                            <button
-                                                className="btn btn-sm btn-success hover:bg-cyan-600 text-slate-900 hover:text-white sm:w-1/4 text-xs md:text-sm tooltip flex"
-                                                onClick={() => handleEditClick(element)}
-                                                data-tip="Editar"
-                                            >
-                                                <FontAwesomeIcon
-                                                    icon={faPenToSquare}
-                                                    className="fa-regular fa-pen-to-square"
-                                                    style={{ fontSize: 16 }}
-                                                />
-                                            </button>
-                                            <button
-                                                className="btn btn-sm btn-error hover:bg-red-900 text-black hover:text-white sm:w-1/4 text-xs md:text-sm tooltip flex"
-                                                onClick={() => getPacienteID(element.pk_idPaciente)}
-                                                data-tip="Eliminar Paciente"
-                                            >
-                                                <FontAwesomeIcon
-                                                    icon={faCircleXmark}
-                                                    className="fa-regular fa-circle-xmark"
-                                                    style={{ color: 'white', fontSize: 16 }}
-                                                />
-                                            </button>
-                                        </div>
-                                    </td>
-
-                                </tr>
-
-
-
+                                <Paciente element={element} key={nanoid()} />
                             ))}
-
-
                         </tbody>
                     </table>
                 </div>
@@ -378,9 +518,14 @@ function PagePacientes() {
                 />
             )}
 
-
-
-
+            {isModalNewCita && (
+                <ModalNewCita
+                    isModalOpen={isModalNewCita}
+                    onClose={closeModal}
+                    onAgregar={getCitas}
+                    paciente={paciente}
+                />
+            )}
 
         </>
     )
