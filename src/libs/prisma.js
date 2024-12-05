5300b8cd23969dced2762dc7c1161196520da8a2
abecerraguz <alejandrobecerrabecerra73@gmail.com>
2024-12-04 22:31:09 -0300

    Se camia libs/prisma.js

--- src/libs/prisma.js
+++ src/libs/prisma.js
@@ -1,19 +1,19 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-// export const prisma = new PrismaClient()
+export const prisma = new PrismaClient()
 
 // para importar datos a la base de datos
 
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 
-// Prevenir múltiples instancias en desarrollo
-const globalForPrisma = global;
+// // Prevenir múltiples instancias en desarrollo
+// const globalForPrisma = global;
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+// const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   globalForPrisma.prisma = prisma;
+// }
 
-export default prisma;
+// export default prisma;
 
