fa61d5e926295e407270baa54682224640d5166c
abecerraguz <alejandrobecerrabecerra73@gmail.com>
2024-12-10 17:09:17 -0300

    Se configura next.config para que no muestre los log al hacer el build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true, // Activa el SWC minifier
+    compiler: {
+      removeConsole: {
+        exclude: ['error', 'warn'], // Opcional: conserva ciertos métodos de consola
+      },
+    },
+  };
 
 module.exports = nextConfig
